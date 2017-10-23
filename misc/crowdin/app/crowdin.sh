47658426ef9836fee0ef81eaf44a7377707f6b2a
Benoît Guérout <bguerout@gmail.com>
2017-10-23 20:51:13 +0200

    Add script of higher level

--- misc/crowdin/app/crowdin.sh
+++ misc/crowdin/app/crowdin.sh
@@ -5,8 +5,6 @@ set -ue -o pipefail
 pushd `dirname $0` > /dev/null
 BASEDIR=`pwd`
 popd > /dev/null
-JOB="UPLOAD"
-DRY_RUN=""
 
 while [[ $# > 1 ]]
 do
@@ -20,10 +18,6 @@ case $key in
         VERSION="$2"
         shift
     ;;
-    -d|--dry-run)
-        DRY_RUN="--dryrun"
-        shift
-    ;;
     *)
      echo "Invalid arguments"
      exit 1
@@ -33,15 +27,16 @@ shift
 done
 
 case "$JOB" in
-UPLOAD)
+UPLOAD_FILES_TO_TRANSLATE)
     pushd /app > /dev/null
         crowdin upload sources
     popd > /dev/null
     ;;
-BUILD_CROWDIN_PROJECT)
+BUILD_TRANSLATIONS)
     curl "https://api.crowdin.com/api/project/kowak/export?key=2bc319a7164349031f4f38d311e49daf&json=true"
     ;;
-DOWNLOAD)
+DOWNLOAD_TRANSLATIONS)
+    # crowdin download seems not to work so do the same with the api
     ZIP_FILE=$(mktemp)
     curl -o ${ZIP_FILE} "https://api.crowdin.com/api/project/kowak/download/all.zip?key=2bc319a7164349031f4f38d311e49daf&language=all"
     unzip -d /translations ${ZIP_FILE}
